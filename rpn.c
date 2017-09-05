80ec3b965c767b72e2461d6791b459bab0302ab7
Hanna Lindwall <dic14hli@alfa-9.student.lth.se>
2017-09-05 15:57:10 +0200

    switch

--- rpn.c
+++ rpn.c
@@ -1,26 +1,39 @@
 # include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main()
 {
 int check, count=0;
-char digits[10]="";
-char input;
+int digits[10]= {0};
+int input;
+int sum;
+int divv;
+int mul;
+int sub;
+
 printf("Enter shit here: \n");
 while (1==1) {
-  input = (char) getchar();
-
+  input = getchar();
   if (count==9) {
-    //digits={0};
     printf("Error at: %c \n", input);
     count=0;
+    memset(digits, 0, sizeof(digits));
   } else {
-    if (strcmp(input," ")==0 ) {
-
+    if (isdigit(input)) {
+      digits[count] = input;
+      count++;
+    } else {
+      switch (input) {
+        case '+' : printf("blbla1\n"); break;
+        case '-' : printf("blbla2\n"); break;
+        case '*' : printf("blbla3\n"); break;
+        case '/' : printf("blbla4\n"); break;
+        default : printf("blbla5\n"); break;
+      }
     }
-    printf("You wrote: %c \n", input);
-    digits[count]=input;
-    count++;
   }
+
 }
 
 return 0;
