acdcac2e23883366ae5fb39824c24ba505b032aa
HannaLindwall <hanna.lindwall93@gmail.com>
2017-09-14 20:10:06 +0200

    done

--- rpn.c
+++ rpn.c
@@ -2,39 +2,74 @@
 #include <string.h>
 #include <ctype.h>
 
-int main()
-{
-int check, count=0;
+int main() {
+int line=1;
+int count=0;
 int digits[10]= {0};
 int input;
-int sum;
-int divv;
-int mul;
-int sub;
-
-printf("Enter shit here: \n");
+int temp = 0;
+int n=1;
 while (1==1) {
-  input = getchar();
-  if (count==9) {
-    printf("Error at: %c \n", input);
-    count=0;
-    memset(digits, 0, sizeof(digits));
-  } else {
-    if (isdigit(input)) {
-      digits[count] = input;
-      count++;
-    } else {
-      switch (input) {
-        case '+' : printf("blbla1\n"); break;
-        case '-' : printf("blbla2\n"); break;
-        case '*' : printf("blbla3\n"); break;
-        case '/' : printf("blbla4\n"); break;
-        default : printf("blbla5\n"); break;
+  while ((input=getchar())!=EOF) {
+      if (isdigit(input)) {
+        while (isdigit(input)) {
+          input=input-'0';
+          temp=(temp*n)+input;
+          n=10;
+          input = getchar();
+        }
+        n=1;
+        if (count==10) {
+        printf("line %d: error at %d\n", line, temp);
+        break;
+        } else {
+          digits[count] = temp;
+          count++;
+        }
+        temp=0;
       }
-    }
+      if (input=='!') {
+        printf("line %d: error at %c\n", line, input);
+        break;
+      } else if (input=='+' || input=='-'||input=='*'||input=='/') {
+        if (digits[count-1]==0) {
+          printf("line %d: error at /\n", line);
+          break;
+        }if (count>=2) {
+          switch (input) {
+            case '+' : digits[count-2]=digits[count-2]+digits[count-1]; count--; break;
+            case '-' : digits[count-2]=digits[count-2]-digits[count-1]; count--; break;
+            case '*' : digits[count-2]=digits[count-2]*digits[count-1]; count--; break;
+            case '/' : digits[count-2]=digits[count-2]/digits[count-1]; count--; break;
+            default : printf("invalid input\n"); break;
+          }
+        } else {
+          printf("line %d: error at %c\n", line, input);
+          break;
+        }
+
+      }
+      if (input=='\n') {
+        if (count != 1) {
+          printf("line %d: error at \\n\n", line);
+          break;
+        } else if( count==1) {
+          printf("line %d: %d\n", line, digits[0]);
+          break;
+          count=0;
+        }
+      }
+  }
+  temp=0;
+  line++;
+  count=0;
+  if(input==EOF) {
+    break;
+  }
+  while (input!='\n') {
+    input=getchar();
   }
 
 }
-
 return 0;
 }
