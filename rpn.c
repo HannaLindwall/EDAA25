ea6abbfd3339d6ea00b2844612b6d5a956d82378
HannaLindwall <hanna.lindwall93@gmail.com>
2017-09-17 20:22:13 +0200

    word done

--- rpn.c
+++ rpn.c
@@ -10,66 +10,69 @@ int input;
 int temp = 0;
 int n=1;
 while (1==1) {
-  while ((input=getchar())!=EOF) {
-      if (isdigit(input)) {
-        while (isdigit(input)) {
-          input=input-'0';
-          temp=(temp*n)+input;
-          n=10;
-          input = getchar();
-        }
-        n=1;
-        if (count==10) {
-        printf("line %d: error at %d\n", line, temp);
-        break;
-        } else {
-          digits[count] = temp;
-          count++;
-        }
-        temp=0;
-      }
-      if (input=='!') {
-        printf("line %d: error at %c\n", line, input);
-        break;
-      } else if (input=='+' || input=='-'||input=='*'||input=='/') {
-        if (digits[count-1]==0) {
-          printf("line %d: error at /\n", line);
-          break;
-        }if (count>=2) {
-          switch (input) {
-            case '+' : digits[count-2]=digits[count-2]+digits[count-1]; count--; break;
-            case '-' : digits[count-2]=digits[count-2]-digits[count-1]; count--; break;
-            case '*' : digits[count-2]=digits[count-2]*digits[count-1]; count--; break;
-            case '/' : digits[count-2]=digits[count-2]/digits[count-1]; count--; break;
-            default : printf("invalid input\n"); break;
-          }
-        } else {
-          printf("line %d: error at %c\n", line, input);
-          break;
-        }
-
-      }
-      if (input=='\n') {
-        if (count != 1) {
-          printf("line %d: error at \\n\n", line);
-          break;
-        } else if( count==1) {
-          printf("line %d: %d\n", line, digits[0]);
-          break;
-          count=0;
-        }
-      }
-  }
-  temp=0;
-  line++;
-  count=0;
-  if(input==EOF) {
-    break;
-  }
-  while (input!='\n') {
-    input=getchar();
-  }
-
+while ((input=getchar())!=EOF) {
+if (isdigit(input)) {
+while (isdigit(input)) {
+input=input-'0';
+temp=(temp*n)+input;
+n=10;
+input = getchar();
+}
+n=1;
+if (count==10) {
+printf("line %d: error at %d\n", line, temp);
+break;
+} else {
+digits[count] = temp;
+count++;
+}
+temp=0;
+}
+if (input=='!') {
+printf("line %d: error at %c\n", line, input);
+break;
+} else if (input=='+' || input=='-'||input=='*'||input=='/') {
+if (digits[count-1]==0) {
+printf("line %d: error at /\n", line);
+break;
+}
+if (count>=2) {
+switch (input) {
+case '+' : digits[count-2]=digits[count-2]+digits[count-1]; count--;
+break;
+case '-' : digits[count-2]=digits[count-2]-digits[count-1]; count--;
+break;
+case '*' : digits[count-2]=digits[count-2]*digits[count-1]; count--;
+break;
+case '/' : digits[count-2]=digits[count-2]/digits[count-1]; count--;
+break;
+default : printf("invalid input\n"); break;
+}
+} else {
+printf("line %d: error at %c\n", line, input);
+break;
+}
+}
+if (input=='\n') {
+if (count != 1) {
+printf("line %d: error at \\n\n", line);
+break;
+} else if ( count==1) {
+printf("line %d: %d\n", line, digits[0]);
+break;
+count=0;
+}
+}
+}
+temp=0;
+line++;
+count=0;
+if (input==EOF) {
+break;
+}
+while (input!='\n') {
+input=getchar();
+}
 }
 return 0;
 }
---
+++ word.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+int main (){
+int input;
+char* word = malloc(sizeof(int));
+char* longest_word = malloc(sizeof(int));
+int count=0;
+int longest=0;
+while ((input=getchar())!=EOF) {
+while (isalpha(input)) {
+word[count]=input;
+count++;
+word = realloc(word, (count+1) * sizeof(int));
+input=getchar();
+}
+if (count>longest && count!=0) {
+longest_word=realloc(longest_word, (count+1) * sizeof(int));
+memcpy(longest_word, word, (count+1) * sizeof(int));
+longest=count;
+}
+count=0;
+}
+printf("%d characters in longest word: %.*s\n", longest, longest, longest_word);
+free(word);
+free(longest_word);
+return 0;
+}
